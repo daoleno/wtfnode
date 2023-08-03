29a30c708835be71a585d308fffed169e69ef287
daoleno <daodaoonline@gmail.com>
2023-08-03 11:23:02 +0800

    fix: handle batch correctly

--- provider.go
+++ provider.go
@@ -10,23 +10,21 @@ import (
 	"net/url"
 )
 
-// Provider is a struct that holds the URL of the backend service it should forward requests to.
 type Provider struct {
 	URL *url.URL
 }
 
-// NewProvider is a constructor function that creates a new Provider.
 func NewProvider(urlStr string) (*Provider, error) {
 	u, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, err
 	}
+
 	return &Provider{
 		URL: u,
 	}, nil
 }
 
-// Structure for JSON-RPC response
 type JSONRPCResponse struct {
 	Version string          `json:"jsonrpc"`
 	Result  json.RawMessage `json:"result,omitempty"`
@@ -34,31 +32,62 @@ type JSONRPCResponse struct {
 	ID      int             `json:"id"`
 }
 
-// Structure for JSON-RPC error
 type JSONRPCError struct {
 	Code    int             `json:"code"`
 	Message string          `json:"message"`
 	Data    json.RawMessage `json:"data,omitempty"`
 }
 
+type JSONRPCBatchResponse []JSONRPCResponse
+
+type JSONRPCRequest struct {
+	ID     int             `json:"id"`
+	Method string          `json:"method"`
+	Params json.RawMessage `json:"params"`
+}
+
+type JSONRPCBatchRequest []JSONRPCRequest
+
+// Check if request is a batch JSON-RPC request or not
+func isBatchJSONRPCRequest(req *http.Request) (bool, error) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return false, err
+	}
+
+	req.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	var batchReq JSONRPCBatchRequest
+	err = json.Unmarshal(body, &batchReq)
+
+	if err != nil {
+		// Error occurred. Treat as not a batch request.
+		return false, nil
+	}
+
+	// It is a batch JSON-RPC request.
+	return true, nil
+}
+
 // Send JSON-RPC request and handle errors in response
 func (p *Provider) SendRequest(req *http.Request) (*http.Response, error) {
-	// Create a new HTTP request
+	isBatch, err := isBatchJSONRPCRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
 	newReq, err := http.NewRequest(req.Method, p.URL.String(), req.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// Copy headers
 	newReq.Header = req.Header
 
-	// Send the new request
 	resp, err := http.DefaultClient.Do(newReq)
 	if err != nil {
 		return nil, err
 	}
 
-	// Check the response for error conditions
 	if resp.StatusCode != http.StatusOK {
 		var buf bytes.Buffer
 		_, err := buf.ReadFrom(resp.Body)
@@ -68,50 +97,54 @@ func (p *Provider) SendRequest(req *http.Request) (*http.Response, error) {
 		return nil, errors.New(buf.String())
 	}
 
-	// Check if response is encoded with gzip
 	if resp.Header.Get("Content-Encoding") == "gzip" {
-		// Decode response body as gzip
 		gzipReader, err := gzip.NewReader(resp.Body)
 		if err != nil {
 			return nil, err
 		}
 		defer gzipReader.Close()
 
-		// delete the Content-Encoding header
 		resp.Header.Del("Content-Encoding")
 
-		// Read the decompressed response body
 		body, err := io.ReadAll(gzipReader)
 		if err != nil {
 			return nil, err
 		}
 
-		// Set the decompressed body as the new response body
 		resp.Body = io.NopCloser(bytes.NewReader(body))
 	}
 
-	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// Close the response body to prevent resource leaks
 	resp.Body.Close()
 
-	// Parse the JSON-RPC response
-	var jsonResp JSONRPCResponse
-	err = json.Unmarshal(body, &jsonResp)
-	if err != nil {
-		return nil, err
+	if isBatch {
+		var batchJsonResp JSONRPCBatchResponse
+		err = json.Unmarshal(body, &batchJsonResp)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, resp := range batchJsonResp {
+			if resp.Error != nil {
+				return nil, errors.New(resp.Error.Message)
+			}
+		}
+	} else {
+		var jsonResp JSONRPCResponse
+		err = json.Unmarshal(body, &jsonResp)
+		if err != nil {
+			return nil, err
+		}
+
+		if jsonResp.Error != nil {
+			return nil, errors.New(jsonResp.Error.Message)
+		}
 	}
 
-	// Check if an error exists
-	if jsonResp.Error != nil {
-		return nil, errors.New(jsonResp.Error.Message)
-	}
-
-	// Create a new response with the same values as the original response
 	newResp := http.Response{
 		Status:        resp.Status,
 		StatusCode:    resp.StatusCode,
@@ -120,9 +153,8 @@ func (p *Provider) SendRequest(req *http.Request) (*http.Response, error) {
 		ProtoMinor:    resp.ProtoMinor,
 		ContentLength: int64(len(body)),
 		Header:        resp.Header,
-		Body:          io.NopCloser(bytes.NewReader(body)), // Create a new ReadCloser from the body
+		Body:          io.NopCloser(bytes.NewReader(body)),
 	}
 
-	// Return the new response
 	return &newResp, nil
 }
